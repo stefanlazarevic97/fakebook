4c37978373176322343d81c24af5d632ea07fd0d
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-13 13:25:44 -0700

    add LoginFormPage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import NewsFeed from './components/NewsFeed/NewsFeed';
 
 function App() {
     return (
         <>
-            <h1>Hello world!</h1>
+            <Switch>
+                <Route exact path="/login">
+                    <LoginFormPage />
+                </Route>
+
+                <Route path="/">
+                    <NewsFeed />
+                </Route>
+            </Switch>
         </>
     );
 }
---
+++ frontend/src/components/NewsFeed/NewsFeed.jsx
@@ -0,0 +1,9 @@
+import './NewsFeed.css';
+
+const NewsFeed = () => {
+    return (
+        <h1>News Feed</h1>
+    )
+}
+
+export default NewsFeed;
