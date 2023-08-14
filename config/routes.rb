65ef884ece4f590f16bee2e4a14bfbbf962dfbd8
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-14 16:24:04 -0700

    .gitignore public

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
         resources :users, only: [:create, :show, :update, :destroy]
         resource :session, only: [:show, :create, :destroy]
     end
+
+    get '*path', 
+        to: 'static_pages#frontend', 
+        constraints: lambda { |req| !req.xhr? && req.format.html? } 
+
+    root to: 'static_pages#frontend'
 end
