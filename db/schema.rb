fba230e163d7c5cc424cc930b4e24e7f51fffb11
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-15 10:30:27 -0700

    prepare for hosting v2

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,33 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_15_035713) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.bigint "author_id", null: false
-    t.text "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
-  end
+    create_table "posts", force: :cascade do |t|
+        t.bigint "author_id", null: false
+        t.text "body", null: false
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["author_id"], name: "index_posts_on_author_id"
+    end
 
-  create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.text "bio"
-    t.string "email"
-    t.string "phone"
-    t.string "session_token", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["first_name"], name: "index_users_on_first_name"
-    t.index ["last_name"], name: "index_users_on_last_name"
-    t.index ["phone"], name: "index_users_on_phone", unique: true
-    t.index ["session_token"], name: "index_users_on_session_token", unique: true
-  end
+    create_table "users", force: :cascade do |t|
+        t.string "first_name", null: false
+        t.string "last_name", null: false
+        t.text "bio"
+        t.string "email"
+        t.string "phone"
+        t.string "session_token", null: false
+        t.string "password_digest", null: false
+        t.datetime "created_at", null: false
+        t.datetime "updated_at", null: false
+        t.index ["email"], name: "index_users_on_email", unique: true
+        t.index ["first_name"], name: "index_users_on_first_name"
+        t.index ["last_name"], name: "index_users_on_last_name"
+        t.index ["phone"], name: "index_users_on_phone", unique: true
+        t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    end
 
-  add_foreign_key "posts", "users", column: "author_id"
+    add_foreign_key "posts", "users", column: "author_id"
 end
